b0de77c3b55330025c1e8007da33795014d39b91
FDAPADILHA <filipe.amaral.padilha@gmail.com>
2020-04-27 12:43:49 -0300

    fourth commit

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
 function validar(){
-    
+    /*if(x==0){
+        console.log("retornou false");
+        return false;
+    }
+    else{
+        console.log("retornou false");
+        return true;
+    }*/
+    alert("OI");
+    return false;
 }
