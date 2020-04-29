b2befee3e36783261ef7b546c517f9a9b338918c
FDAPADILHA <filipe.amaral.padilha@gmail.com>
2020-04-28 22:31:23 -0300

    sixth

--- scripts.js
+++ scripts.js
@@ -1,8 +1,23 @@
 function validar(){
-    if(document.form.nome.value=="" || document.form.nome.value== null){
+    if(document.form.nome.value == "" || document.form.nome.value == null){
         alert("Você não preencheu o campo Nome");
+        document.form.nome.focus();
         return false;
-    }else{
-        return true;
     }
+    if(document.form.preco.value == "" || document.form.preco.value == null){
+        alert("Você não preencheu o campo Preço");
+        document.form.preco.focus();
+        return false;
+    }
+    if(document.form.preco.value < 0){
+        alert("Valor inserido inválido");
+        document.form.preco.focus();
+        return false;
+    }
+    
+    
+    
+else{
+    return true;
+}
 }
