27cfe450f3ead73958b9c74bd81838db6526caf5
FDAPADILHA <filipe.amaral.padilha@gmail.com>
2020-04-29 22:54:10 -0300

    seventh

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
 function validar(){
+    if(document.form.select.options[select.selectedIndex].value == "" || document.form.select.options[select.selectedIndex].value == null){
+        alert("Você não selecionou uma Categoria");
+        document.form.select.focus();
+        return false;
+    }
     if(document.form.nome.value == "" || document.form.nome.value == null){
         alert("Você não preencheu o campo Nome");
         document.form.nome.focus();
@@ -9,15 +14,22 @@ function validar(){
         document.form.preco.focus();
         return false;
     }
-    if(document.form.preco.value < 0){
+    if(document.form.preco.value <= 0){
         alert("Valor inserido inválido");
         document.form.preco.focus();
         return false;
     }
-    
-    
-    
-else{
-    return true;
-}
+    if(document.form.quantidade.value == "" || document.form.quantidade.value == null){
+        alert("Você não preencheu o campo Quantidade");
+        document.form.quantidade.focus();
+        return false;
+    }
+    if(document.form.quantidade.value <= 0){
+        alert("Valor inserido inválido");
+        document.form.quantidade.focus();
+        return false;
+    }
+    else{
+        return true;
+    }
 }
